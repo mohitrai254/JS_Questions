f685137d26b77ccb0244baef774515a1e9fbf9c8
Mohit <mohitrai254@gmail.com>
2025-07-29 08:03:26 +0530

    new

--- Q22_removeDuplicate.js
+++ Q22_removeDuplicate.js
@@ -1,1 +1,17 @@
-//	Write a function to remove the duplicate from 2 arrays, let a = [1,2,3,4,5] let b = [5,3,7,8,9]
+let a = [1, 2, 3, 4, 5];
+let b = [5, 3, 7, 8, 9];
+
+let arr = [];
+
+for (let i = 0; i < a.length; i++) {
+  arr.push(a[i]);
+}
+
+for (let j = 0; j < b.length; j++) {
+  if (!arr.includes(b[j])) {
+    arr.push(b[j]);
+  }
+}
+
+// let arr = [...new Set([...a, ...b])];
+console.log(arr);
